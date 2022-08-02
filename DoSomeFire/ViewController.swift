8f3716a10d653fd834e0a667d0a0800ab4d05bf7
Maksewsha <makasiz.makson@gmail.com>
2022-08-02 15:34:26 +0300

    Initial Commit

---
+++ DoSomeFire/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DoSomeFire
+//
+//  Created by admin on 02.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
