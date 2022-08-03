3a634efabba5bccf68007b687ffb57ec27630164
Maksewsha <makasiz.makson@gmail.com>
2022-08-03 15:49:49 +0300

    Added Firebase logic with auth and database

--- DoSomeFire/Controllers/LoginViewController.swift
+++ DoSomeFire/Controllers/LoginViewController.swift
@@ -4,23 +4,97 @@
 //
 //  Created by admin on 02.08.2022.
 //
-
+import FirebaseAuth
+import FirebaseDatabase
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    
+    private let segueIdentifier = "tasksSegue"
+    private var ref: DatabaseReference!
 
     @IBOutlet weak var passTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var doesntExistLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        ref = Database.database().reference(withPath: "users")
+        doesntExistLabel.alpha = 0
+        
+        Auth.auth().addStateDidChangeListener { [weak self] auth, user in
+            if user != nil{
+                self?.performSegue(withIdentifier: (self?.segueIdentifier)!, sender: nil)
+            }
+        }
+    }
+    
+    private func displayWarningLabel(withText text: String){
+        doesntExistLabel.text = text
+        UIView.animate(withDuration: 3, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseInOut) { [weak self] in
+            self?.doesntExistLabel.alpha = 1
+        } completion: { [weak self] complete in
+            self?.doesntExistLabel.alpha = 0
+        }
+
     }
 
 
     @IBAction func registerTapped(_ sender: UIButton) {
+        guard let email = emailTextField.text, let password = passTextField.text, email != "", password != "" else {
+            displayWarningLabel(withText: "Data is incorrect")
+            return
+        }
+        
+        Auth.auth().createUser(withEmail: email, password: password) { [weak self] user, error in
+            guard error == nil, user != nil else {
+                print(error?.localizedDescription)
+                return
+            }
+            
+            let userRef = self?.ref.child((user?.user.uid)!)
+            userRef!.setValue(["email" : user?.user.email])
+            
+        }
     }
+    
+    
     @IBAction func loginTapped(_ sender: UIButton) {
+        guard let email = emailTextField.text, let password = passTextField.text, email != "", password != "" else {
+            displayWarningLabel(withText: "Data is incorrect")
+            return
+        }
+        
+        Auth.auth().signIn(withEmail: email, password: password) { [weak self] user, error in
+            if error != nil{
+                self?.displayWarningLabel(withText: "Error occured")
+                return
+            }
+            if user != nil {
+                self?.performSegue(withIdentifier: "tasksSegue", sender: nil)
+                return
+            }
+            
+            self?.displayWarningLabel(withText: "No such user")
+        }
     }
+    
+    @IBAction func signOutTapped(_ sender: UIBarButtonItem){
+        do{
+            try Auth.auth().signOut()
+        } catch let error as NSError{
+            
+        }
+        dismiss(animated: true)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        emailTextField.text = ""
+        passTextField.text = ""
+    }
+    
+    
 }
 
--- DoSomeFire/Controllers/TasksViewController.swift
+++ DoSomeFire/Controllers/TasksViewController.swift
@@ -6,28 +6,95 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseDatabase
+import FirebaseAuth
+
 
 class TasksViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    private var user: MyUser!
+    private var reference: DatabaseReference!
+    private var tasks = Array<Task>()
     @IBOutlet weak var tableView: UITableView!
     
     @IBAction func barButtonTapped(_ sender: UIBarButtonItem) {
+        let alertController = UIAlertController(title: "New task", message: "Add new task", preferredStyle: .alert)
+        alertController.addTextField()
+        let saveAction = UIAlertAction(title: "Save", style: .default) { [weak self] action in
+            guard let textField = alertController.textFields?.first, textField.text != "" else { return }
+            
+            let task = Task(title: textField.text!, userId: (self?.user.uid)!)
+            let taskRef = self?.reference.child(task.title.lowercased())
+            taskRef?.setValue(task.convertToDict)
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default)
+        alertController.addAction(saveAction)
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true)
     }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
+        guard let currentUser = Auth.auth().currentUser else { return }
+        user = MyUser(user: currentUser)
+        reference = Database.database().reference(withPath: "users").child(String(user.uid)).child("tasks")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        reference.observe(.value) { [weak self] snapshot in
+            var _tasks = Array<Task>()
+            for item in snapshot.children{
+                let task = Task(snapshot: item as! DataSnapshot)
+                _tasks.append(contentsOf: [task])
+            }
+            self?.tasks = _tasks
+            self?.tableView.reloadData()
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        reference.removeAllObservers()
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let taskTitle = tasks[indexPath.row].title
+        cell.textLabel?.text = taskTitle
+        toggleCompletion(cell, isCompleted: tasks[indexPath.row].completed)
         return cell
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
     }
     
     
-
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+            let task = tasks[indexPath.row]
+            task.ref?.removeValue()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let cell = tableView.cellForRow(at: indexPath) else { return }
+        let task = tasks[indexPath.row]
+        let isCompleted = !task.completed
+        toggleCompletion(cell, isCompleted: isCompleted)
+        task.ref?.updateChildValues(["completed": isCompleted])
+    }
+    
+    private func toggleCompletion(_ cell: UITableViewCell, isCompleted: Bool){
+        cell.accessoryType = isCompleted ? .checkmark : .none
+    }
 }
